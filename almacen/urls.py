29ff0194b984a05ebe82b6b4f878f92686550674
Paolo Soncco <paholo2054@gmail.com>
2022-12-03 11:24:49 -0500

    update urls

--- almacen/urls.py
+++ almacen/urls.py
@@ -1,7 +1,28 @@
-from .views import CategoriasView
+from .views import (
+  CategoriasView, UpdateCategoriasView,
+  ProductosView, UpdateProductosView,
+  ClientesView, UpdateClientesView,
+  OrdenesView, UpdateOrdenesView,
+  DetallesOrdenView, UpdateDetallesOrdenView,
+  PagosView, UpdatePagosView,
+  BoletasPagoView, UpdateBoletasPagoView,
+)
 from django.urls import path
 
 
 urlpatterns = [
   path('categorias', CategoriasView.as_view()),
+  path('categorias/<pk>', UpdateCategoriasView.as_view()),
+  path('productos', ProductosView.as_view()),
+  path('productos/<pk>', UpdateProductosView.as_view()),
+  path('clientes', ClientesView.as_view()),
+  path('clientes/<pk>', UpdateClientesView.as_view()),
+  path('ordenes', OrdenesView.as_view()),
+  path('ordenes/<pk>', UpdateOrdenesView.as_view()),
+  path('detalles_orden', DetallesOrdenView.as_view()),
+  path('detalles_orden/<pk>', UpdateDetallesOrdenView.as_view()),
+  path('pagos', PagosView.as_view()),
+  path('pagos/<pk>', UpdatePagosView.as_view()),
+  path('boletas_pago', BoletasPagoView.as_view()),
+  path('boletas_pago/<pk>', UpdateBoletasPagoView.as_view()),
 ]
--- almacen/views.py
+++ almacen/views.py
@@ -10,7 +10,13 @@ from .serializers import (
 )
 
 from .models import (
-  CategoriasModel
+  CategoriasModel,
+  ProductosModel,
+  ClientesModel,
+  OrdenesModel,
+  DetallesOrdenModel,
+  PagosModel,
+  BoletasPagoModel,
 )
 
 # Create your views here.
@@ -18,3 +24,71 @@ from .models import (
 class CategoriasView(generics.ListCreateAPIView):
   queryset = CategoriasModel.objects.all()
   serializer_class = CategoriasSerializer
+
+class UpdateCategoriasView(generics.RetrieveUpdateDestroyAPIView):
+  queryset = CategoriasModel.objects.all()
+  serializer_class = CategoriasSerializer
+  http_method_names  = ['get','put','delete']
+
+class ProductosView(generics.ListCreateAPIView):
+  queryset = ProductosModel.objects.all()
+  serializer_class = ProductosSerializer
+
+class UpdateProductosView(generics.RetrieveUpdateDestroyAPIView):
+  queryset = ProductosModel.objects.all()
+  serializer_class = ProductosSerializer
+  http_method_names  = ['get','put','delete']
+
+class ClientesView(generics.ListCreateAPIView):
+  queryset = ClientesModel.objects.all()
+  serializer_class = ClientesSerializer
+
+class UpdateClientesView(generics.RetrieveUpdateDestroyAPIView):
+  queryset = ClientesModel.objects.all()
+  serializer_class = ClientesSerializer
+  http_method_names  = ['get','put','delete']
+
+class OrdenesView(generics.ListCreateAPIView):
+  queryset = OrdenesModel.objects.all()
+  serializer_class = OrdenesSerializer
+
+class UpdateOrdenesView(generics.RetrieveUpdateDestroyAPIView):
+  queryset = OrdenesModel.objects.all()
+  serializer_class = OrdenesSerializer
+  http_method_names  = ['get','put','delete']
+
+class DetallesOrdenView(generics.ListCreateAPIView):
+  queryset = DetallesOrdenModel.objects.all()
+  serializer_class = DetallesOrdenSerializer
+
+class UpdateDetallesOrdenView(generics.RetrieveUpdateDestroyAPIView):
+  queryset = DetallesOrdenModel.objects.all()
+  serializer_class = DetallesOrdenSerializer
+  http_method_names  = ['get','put','delete']
+
+class PagosView(generics.ListCreateAPIView):
+  queryset = PagosModel.objects.all()
+  serializer_class = PagosSerializer
+
+class UpdatePagosView(generics.RetrieveUpdateDestroyAPIView):
+  queryset = PagosModel.objects.all()
+  serializer_class = PagosSerializer
+  http_method_names  = ['get','put','delete']
+
+class BoletasPagoView(generics.ListCreateAPIView):
+  queryset = BoletasPagoModel.objects.all()
+  serializer_class = BoletasPagoSerializer
+
+class UpdateBoletasPagoView(generics.RetrieveUpdateDestroyAPIView):
+  queryset = BoletasPagoModel.objects.all()
+  serializer_class = BoletasPagoSerializer
+  http_method_names  = ['get','put','delete']
+
+# class CategoriasView(generics.ListCreateAPIView):
+#   queryset = CategoriasModel.objects.all()
+#   serializer_class = CategoriasSerializer
+
+# class UpdateCategoriasView(generics.RetrieveUpdateDestroyAPIView):
+#   queryset = CategoriasModel.objects.all()
+#   serializer_class = CategoriasSerializer
+#   http_method_names  = ['get','put','delete']
